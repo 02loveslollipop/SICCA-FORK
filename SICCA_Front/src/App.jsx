a494fdb64891b413f8e570863f8565aed4b50284
valensoto486 <valensoto486@gmail.com>
2024-10-08 23:33:41 -0500

    Base para el Dashboard del admin

--- SICCA_Front/src/App.jsx
+++ SICCA_Front/src/App.jsx
@@ -1,9 +1,13 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 import './styles/LoginPage.css'
+//import LoginPage from './pages/LoginPage';
+import Dashboard from './pages/Dashboard';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import LoginPage from './pages/LoginPage';
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
   console.log('LoginPage component is being rendered');
   return (
     <>
