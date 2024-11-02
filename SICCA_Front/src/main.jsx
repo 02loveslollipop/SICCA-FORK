ec0b44916129b5d07d9510fb376fceca62346635
valensoto486 <valensoto486@gmail.com>
2024-11-01 20:01:06 -0500

    integracion paginas en el dashboard

--- SICCA_Front/src/main.jsx
+++ SICCA_Front/src/main.jsx
@@ -1,13 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import App from './App.jsx'
-import Dashboard from './pages/DashboardAdmin.jsx'
+import App from './App.jsx'
 //import './index.css'
-//import DashboardAdmin from './pages/DashboardAdmin.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Dashboard/>
+    <App/>
   </StrictMode>,
 )
